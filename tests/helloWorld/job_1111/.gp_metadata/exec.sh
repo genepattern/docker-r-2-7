4903167e337ce969c414345fb0d35c62468a7319
ted Liefeld <liefeld@broadinstitute.org>
2018-01-10 08:59:49 -0800

    update R_HOME location

--- tests/helloWorld/job_1111/.gp_metadata/exec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-java -cp /build -DR_suppress=true -DR_HOME=/usr/local/lib64/R -Dr_flags="--no-save --quiet --slave --no-restore" RunR /Users/liefeld/GenePattern/gp_dev/docker/docker-r-2-7/tests/helloWorld/src/hello.R hello
-
- 
