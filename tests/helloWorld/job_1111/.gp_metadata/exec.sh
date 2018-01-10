0b48ca8eab44a925cf8864c498f808e7120dfa90
ted Liefeld <liefeld@broadinstitute.org>
2018-01-10 08:59:15 -0800

    bad revision

--- tests/helloWorld/job_1111/.gp_metadata/exec.sh
+++ tests/helloWorld/job_1111/.gp_metadata/exec.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-java -cp /build -DR_HOME=/packages/R-2.7.2/ -Dr_flags="--no-save --quiet --slave --no-restore" RunR /Users/liefeld/GenePattern/gp_dev/docker/docker-r-2-7/tests/helloWorld/src/hello.R hello
+java -cp /build -DR_suppress=true -DR_HOME=/usr/local/lib64/R -Dr_flags="--no-save --quiet --slave --no-restore" RunR /Users/liefeld/GenePattern/gp_dev/docker/docker-r-2-7/tests/helloWorld/src/hello.R hello
 
  
